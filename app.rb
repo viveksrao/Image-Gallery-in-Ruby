171f1f929016e9e53394ffe2708eb9974dd92b60
Vivekanand Rao <vivek.rao@teachscape.com>
2015-06-17 14:51:23 -0700

    Added Basic Authentication.

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 class App < Sinatra::Base
   get "/" do
+    @auth = authorized?
     @images = Image.all
     haml :index
   end
@@ -9,9 +10,29 @@ class App < Sinatra::Base
     haml :show
   end
 
+  get "/auth" do
+    protected!
+    redirect "/"
+  end
+
   post "/images" do
+    protected!
     @image = Image.new params[:image]
     @image.save
     redirect "/"
   end
+
+  helpers do
+    def protected!
+      return if authorized?
+      headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+      halt 401, "Not Authorized\n"
+    end
+
+    def authorized?
+      @auth ||= Rack::Auth::Basic::Request.new(request.env)
+      @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['uidimgallery','pwdimgallery']
+    end
+  end
+
 end
